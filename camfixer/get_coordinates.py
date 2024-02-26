1d6734ff78158bd2166144c5c116911a45efb3b8
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2024-02-26 13:39:48 -0600

    Refactor get_coordinates function to improve readability and remove commented code

--- camfixer/get_coordinates.py
+++ camfixer/get_coordinates.py
@@ -11,7 +11,9 @@ def get_coordinates(line):
 
     Returns:
         tuple: A tuple with the coordinates. e.g. (269.8, -29.0, 264.2, -23.4).
+    
     """
+    
     # pattern = r"G0[0123]X([+-]?\d+\.\d+)Y([+-]?\d+\.\d+)I([+-]?\d+\.\d+)J([+-]?\d+\.\d+)"
     pattern = r"G0[0123]X([+-]?\d+\.\d+)Y([+-]?\d+\.\d+)(I([+-]?\d+\.\d+))?(J([+-]?\d+\.\d+))?"
     match = re.search(pattern, line)
@@ -23,7 +25,20 @@ def get_coordinates(line):
         else:
             return float(x), float(y)
     return None
-
+    
+"""
+    # pattern = r"G0[0123]X([+-]?\d+\.\d+)Y([+-]?\d+\.\d+)I([+-]?\d+\.\d+)J([+-]?\d+\.\d+)"
+    pattern = r"G0[0123]X([+-]?\d+\.\d+)Y([+-]?\d+\.\d+)(I([+-]?\d+\.\d+))?(J([+-]?\d+\.\d+))?"
+    match = re.search(pattern, line)
+    if match:
+        x, y, _, i, _, j = match.groups()
+        # x, y, i, j = match.groups()
+        if i and j:
+            return float(x), float(y)
+        else:
+            return float(x), float(y)
+    return None
+"""
 
 if __name__ == "__main__":
     line = "G03X+269.8Y-29.0I+264.2J-23.4"
